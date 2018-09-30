af64d4e3224ccbc8b369f9a34a7c5bfb5ad78f24
Nano Degree <nanodegree@Nanos-Mac.local>
2018-09-30 14:43:02 +0500

    Initial Commit for FoodXilla

---
+++ FoodXilla/SideMenuView.swift
@@ -0,0 +1,21 @@
+//
+//  SideMenuView.swift
+//  FoodXilla
+//
+//  Created by Nano Degree on 20/09/2018.
+//  Copyright © 2018 Nano Degree. All rights reserved.
+//
+
+import UIKit
+
+class SideMenuView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ HomeCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  HomeCollectionViewCell.swift
+//  FoodXilla
+//
+//  Created by Nano Degree on 19/09/2018.
+//  Copyright © 2018 Nano Degree. All rights reserved.
+//
+
+import UIKit
+
+class HomeCollectionViewCell: UICollectionViewCell {
+    
+}
